cfbbc2e09c0bb457a46feb43b5daebd0140521d8
tony.origogi <tony.origogi@kakaobrain.com>
2022-10-19 14:15:35 +0900

    Add error handling

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
@@ -10,48 +11,51 @@ import 'package:go_router/go_router.dart';
 
 enum AppRoute { home, cart, orders, account, signIn, product }
 
-final goRouter =
-    GoRouter(debugLogDiagnostics: true, initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: AppRoute.home.name,
-      builder: (context, state) => const ProductsListScreen(),
-      routes: [
-        GoRoute(
-            path: 'product/:id',
-            name: AppRoute.product.name,
-            builder: (context, state) {
-              final productId = state.params['id']!;
-              return ProductScreen(productId: productId);
-            }),
-        GoRoute(
-            path: 'cart',
-            name: AppRoute.cart.name,
-            pageBuilder: (context, state) => MaterialPage(
-                key: state.pageKey,
-                fullscreenDialog: true,
-                child: const ShoppingCartScreen())),
-        GoRoute(
-            path: 'orders',
-            name: AppRoute.orders.name,
-            pageBuilder: (context, state) => MaterialPage(
-                key: state.pageKey,
-                fullscreenDialog: true,
-                child: const OrdersListScreen())),
-        GoRoute(
-            path: 'account',
-            name: AppRoute.account.name,
-            pageBuilder: (context, state) => MaterialPage(
-                key: state.pageKey,
-                fullscreenDialog: true,
-                child: const AccountScreen())),
-        GoRoute(
-            path: 'signIn',
-            name: AppRoute.signIn.name,
-            pageBuilder: (context, state) => MaterialPage(
-                key: state.pageKey,
-                fullscreenDialog: true,
-                child: const EmailPasswordSignInScreen(
-                    formType: EmailPasswordSignInFormType.signIn))),
-      ]),
-]);
+final goRouter = GoRouter(
+    debugLogDiagnostics: true,
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+          path: '/',
+          name: AppRoute.home.name,
+          builder: (context, state) => const ProductsListScreen(),
+          routes: [
+            GoRoute(
+                path: 'product/:id',
+                name: AppRoute.product.name,
+                builder: (context, state) {
+                  final productId = state.params['id']!;
+                  return ProductScreen(productId: productId);
+                }),
+            GoRoute(
+                path: 'cart',
+                name: AppRoute.cart.name,
+                pageBuilder: (context, state) => MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: const ShoppingCartScreen())),
+            GoRoute(
+                path: 'orders',
+                name: AppRoute.orders.name,
+                pageBuilder: (context, state) => MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: const OrdersListScreen())),
+            GoRoute(
+                path: 'account',
+                name: AppRoute.account.name,
+                pageBuilder: (context, state) => MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: const AccountScreen())),
+            GoRoute(
+                path: 'signIn',
+                name: AppRoute.signIn.name,
+                pageBuilder: (context, state) => MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: const EmailPasswordSignInScreen(
+                        formType: EmailPasswordSignInFormType.signIn))),
+          ]),
+    ],
+    errorBuilder: (context, state) => const NotFoundScreen());
