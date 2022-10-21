e1212e4eb253ea9118eb9a5c18b8e3f2f9daebb9
tony.origogi <tony.origogi@kakaobrain.com>
2022-10-21 13:59:19 +0900

    Creating repos using abstract classes

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,26 +1,78 @@
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class FakeAuthRepository {
-  Stream<AppUser?> authStateChange() => Stream.value(null);
+abstract class AuthRepository {
+  Stream<AppUser?> authStateChanges();
+  AppUser? get currentUser;
+  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<void> createUserWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+}
 
-  AppUser? get currentUser => null;
-
-  Future<void> signInWithEmailAndPassword(String email, String password) async {
-    // TODO
+class FirebaseAuthRepository implements AuthRepository {
+  @override
+  Stream<AppUser?> authStateChanges() {
+    // TODO: implement authStateChanges
+    throw UnimplementedError();
   }
 
+  @override
+  Future<void> createUserWithEmailAndPassword(String email, String password) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AppUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+}
+
+class FakeAuthRepository implements AuthRepository{
+  Stream<AppUser?> authStateChange() => Stream.value(null);
+
+  @override
+  AppUser? get currentUser => null;
+
+
+  @override
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {}
 
+  @override
   Future<void> signOut() async {}
+
+  @override
+  Stream<AppUser?> authStateChanges() {
+    // TODO: implement authStateChanges
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmilAndPassword
+    throw UnimplementedError();
+  }
 }
 
-final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
-  return FakeAuthRepository();
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  const isFake = String.fromEnvironment('useFakeRepos') == 'true';
+  return isFake ? FakeAuthRepository() : FirebaseAuthRepository();
 });
 
 final authStateChangesProvider = StreamProvider<AppUser?>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
-  return authRepository.authStateChange();
+  return authRepository.authStateChanges();
 });
