1c0fec4ca8caba8b2163e8eaad5d3e8a22b2790a
tony.origogi <tony.origogi@kakaobrain.com>
2022-10-21 16:53:17 +0900

    Adjust AsyncGuard

--- lib/src/features/authentication/presentation/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account_screen_controller.dart
@@ -7,16 +7,19 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
     // sign out
     // if success, set state to data
     // if error, set state to error
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-
-      return true;
-    } catch (e, stt) {
-      state = AsyncValue<void>.error(e, stt);
-      return false;
-    }
+    // try {
+    //   state = const AsyncValue<void>.loading();
+    //   await authRepository.signOut();
+    //   state = const AsyncValue<void>.data(null);
+    //
+    //   return true;
+    // } catch (e, stt) {
+    //   state = AsyncValue<void>.error(e, stt);
+    //   return false;
+    // }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 
   AccountScreenController({required this.authRepository})
