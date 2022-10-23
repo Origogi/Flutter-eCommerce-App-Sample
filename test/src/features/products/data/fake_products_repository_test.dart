ca975baf59663d97c4cf2a21539275a7ebb75e22
tony.origogi <tony.origogi@kakaobrain.com>
2022-10-23 10:46:20 +0900

    Add getproduct() unit test

--- test/src/features/products/data/fake_products_repository_test.dart
+++ test/src/features/products/data/fake_products_repository_test.dart
@@ -3,10 +3,19 @@ import 'package:ecommerce_app/src/features/products/data/fake_products_repositor
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   test('getProductList returns global list', () {
     final productRepository = FakeProductsRepository.instance;
 
     expect(productRepository.getProductsList(), kTestProducts);
+  });
+
+  test('getProduct(1) return first item', () {
+    final productRepository = FakeProductsRepository.instance;
+    expect(productRepository.getProduct('1'), kTestProducts[0]);
+  });
+
+  test('getProduct(100) return null', () {
+    final productRepository = FakeProductsRepository.instance;
+    expect(() => productRepository.getProduct('100'), throwsStateError);
   });
 }
