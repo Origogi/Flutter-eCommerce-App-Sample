42896b074aa1d4fbc23ecbce5a295639d576c395
Origogi <rlawjdxo1212@naver.com>
2022-10-25 00:40:19 +0900

    Add mockRepository

--- test/src/features/authentication/presentation/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account_screen_controller_test.dart
@@ -1,15 +1,33 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account_screen_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
 
 void main() {
   group('AccountSceenController', () {
     test('initial state is AsyncValue', () {
-      final authRepository = FakeAuthRepository();
+      final authRepository = MockAuthRepository();
       final controller =
           AccountScreenController(authRepository: authRepository);
-          expect(controller.debugState, const AsyncData<void>(null));
+      verifyNever(authRepository.signOut);
+      expect(controller.debugState, const AsyncData<void>(null));
     });
+  });
+
+  test('signOut success', () async {
+    // setup
+    final authRepository = MockAuthRepository();
+    when(authRepository.signOut).thenAnswer((_) => Future.value());
+
+    final controller = AccountScreenController(authRepository: authRepository);
+    // run
+    await controller.signOut();
+    // verify
+    verify(authRepository.signOut).called(1);
+    expect(controller.debugState, const AsyncData<void>(null));
   });
 }
