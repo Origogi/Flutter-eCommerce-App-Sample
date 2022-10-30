5cc594e124208fe521073161d47ba104859ac76c
Origogi <rlawjdxo1212@naver.com>
2022-10-30 21:57:14 +0900

    Update

--- test/src/goldens/golden_products_list_test.dart
+++ test/src/goldens/golden_products_list_test.dart
@@ -1,17 +1,38 @@
+import 'dart:ui';
+
 import 'package:ecommerce_app/src/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../robot.dart';
 
 void main() {
-  testWidgets('Golden - products list', (tester) async {
-    final r = Robot(tester);
-    await r.golden.loadRobotoFont();
-    await r.golden.loadMaterialIconFont();
-    await r.pumpMyApp();
-    await r.golden.precacheImages();
-
-    await expectLater(
-        find.byType(MyApp), matchesGoldenFile('products_list.png'));
+  // define all size variants
+  final sizeVariant = ValueVariant<Size>({
+    const Size(300, 600),
+    const Size(600, 800),
+    const Size(1000, 1000),
   });
+  testWidgets(
+    'Golden - products list',
+        (tester) async {
+      final r = Robot(tester);
+      // get the current size
+      final currentSize = sizeVariant.currentValue!;
+      // use it to set the surface size
+      await r.golden.setSurfaceSize(currentSize);
+      await r.golden.loadRobotoFont();
+      await r.golden.loadMaterialIconFont();
+      await r.pumpMyApp();
+      await r.golden.precacheImages();
+      await expectLater(
+        find.byType(MyApp),
+        matchesGoldenFile(
+          // generate different output goldens based on the width and height
+          'products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png',
+        ),
+      );
+    },
+    // pass the variant 
+    variant: sizeVariant,
+  );
 }
