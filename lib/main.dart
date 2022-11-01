3b8850d17bbe231e1a0a5154726a5953402c93e4
tony.origogi <tony.origogi@kakaobrain.com>
2022-11-01 17:47:29 +0900

    Local Data persistence with smebast

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
@@ -12,7 +15,12 @@ void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     // turn off the # in the URLs on the web
     // * Entry point of the app
-    runApp(const ProviderScope(child: MyApp()));
+    GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
+    final localCartRepository = await SembastCartRepository.makeDefault();
+    runApp(ProviderScope(overrides: [
+      localCartRepositoryProvider.overrideWithValue(localCartRepository)
+    ], child: const MyApp()));
 
     // * This code will present some error UI if any uncaught exception happens
     FlutterError.onError = (FlutterErrorDetails details) {
