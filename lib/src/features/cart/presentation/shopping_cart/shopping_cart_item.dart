2e9c9c837d99ea23fadb8989fe3525ca269a58fa
Origogi <rlawjdxo1212@naver.com>
2022-11-05 20:29:37 +0900

    Add shoppingcartController and provider

--- lib/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart
+++ lib/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
-import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
---
+++ lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen_controller.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ShoppingCartScreenController extends StateNotifier<AsyncValue<void>> {
+  final CartService cartService;
+
+  ShoppingCartScreenController(this.cartService) : super(const AsyncData(null));
+
+  Future<void> updateItemQuantity(ProductID productId, int quantity) async {
+    state = const AsyncLoading();
+    final updated = Item(productId: productId, quantity: quantity);
+    state = await AsyncValue.guard(() => cartService.setItem(updated));
+  }
+
+  Future<void> removeItemById(ProductID productId) async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() => cartService.removeItemById(productId));
+  }
+}
+
+final shoppingCartScreenControllerProvider =
+    StateNotifierProvider<ShoppingCartScreenController, AsyncValue<void>>(
+        (ref) {
+  return ShoppingCartScreenController(ref.watch(cartServiceProvider));
+});
