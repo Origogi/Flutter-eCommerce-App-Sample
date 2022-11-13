39f74e8aba9fe753e7e5ab3b23b8cf2100887057
Origogi <rlawjdxo1212@naver.com>
2022-11-13 22:06:19 +0900

    Add leaveReviewController

--- lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
+++ lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
@@ -1,8 +1,10 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/constants/breakpoints.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart';
 import 'package:ecommerce_app/src/features/reviews/presentation/product_reviews/product_rating_bar.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
@@ -12,6 +14,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LeaveReviewScreen extends StatelessWidget {
   const LeaveReviewScreen({super.key, required this.productId});
+
   final ProductID productId;
 
   @override
@@ -33,6 +36,7 @@ class LeaveReviewScreen extends StatelessWidget {
 
 class LeaveReviewForm extends ConsumerStatefulWidget {
   const LeaveReviewForm({super.key, required this.productId, this.review});
+
   final ProductID productId;
   final Review? review;
 
@@ -63,6 +67,12 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
 
   @override
   Widget build(BuildContext context) {
+    ref.listen<AsyncValue>(leaveReviewControllerProvider, (_, state) {
+      state.showAlertDialogOnError(context);
+    });
+
+    final state = ref.watch(leaveReviewControllerProvider);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -94,11 +104,15 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
         PrimaryButton(
           text: 'Submit'.hardcoded,
           // TODO: Loading state
-          isLoading: false,
-          onPressed: _rating == 0
+          isLoading: state.isLoading,
+          onPressed: state.isLoading || _rating == 0
               ? null
-              // TODO: submit review
-              : () => showNotImplementedAlertDialog(context: context),
+              : () => ref
+                  .read(leaveReviewControllerProvider.notifier)
+                  .submitReview(
+                      productId: widget.productId,
+                      rating: _rating,
+                      comment: _controller.text),
         )
       ],
     );
